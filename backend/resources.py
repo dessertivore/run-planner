aaeeffb0bb971347305983b4049123688d7b609e
dessertivore <landau.sophie@gmail.com>
2024-02-18 16:01:55 +0000

    Get some functions working

--- backend/resources.py
+++ backend/resources.py
@@ -9,23 +9,7 @@ con = psycopg.connect(
 )
 
 
-def add_run(plan_num, week, day, details):
-    cursor = con.cursor()
-    cursor.execute(
-        """INSERT INTO planner.public.training_plans 
-                VALUES (%s, %s, %s, %s);""",
-        (
-            plan_num,
-            week,
-            day,
-            details,
-        ),
-    )
-    cursor.close()
-    con.commit()
-
-
-def add_plan(plan_num, plan_name):
+def add_plan(plan_num: int, plan_name: str):
     cursor = con.cursor()
     cursor.execute(
         """INSERT INTO planner.public.plan_names (plan_no, plan_name) 
@@ -36,15 +20,72 @@ def add_plan(plan_num, plan_name):
     con.commit()
 
 
-def find_run_func(plan_num, week, day):
+def add_run(plan_num: int, week: int, day: int, details: str):
     cursor = con.cursor()
-    found_run = cursor.execute(
-        """SELECT description_string FROM planner.public.training_plans WHERE
-            plan_no = %s,
-            week_no = %s, 
-            day_no = %s;""",
-        (plan_num, week, day),
+    run_id = cursor.execute(
+        """SELECT MAX(run_id) AS highest_run_id
+        FROM planner.public.training_plans;"""
+    ).fetchone()
+    if run_id == (None,):
+        run_id = 1
+    else:
+        run_id = run_id[0] + 1
+    cursor.execute(
+        """INSERT INTO planner.public.training_plans 
+                VALUES (%s, %s, %s, %s, %s);""",
+        (
+            plan_num,
+            run_id,
+            week,
+            day,
+            details,
+        ),
     )
     cursor.close()
     con.commit()
+
+
+def find_run_func(plan_num: int, week: int, day: int):
+    cursor = con.cursor()
+    found_run = cursor.execute(
+        """SELECT description_string FROM planner.public.training_plans WHERE
+            plan_no = %s
+            AND week_no = %s 
+            AND day_no = %s;""",
+        (
+            plan_num,
+            week,
+            day,
+        ),
+    ).fetchall()
+    cursor.close()
+    con.commit()
     return found_run
+
+
+def find_plan_func(search_plan: int | str) -> list | None:
+    """
+    Search for all plans which match search parameter in database.
+
+    Args:
+        search_plan (int | str): enter plan ID or plan name
+
+    Returns:
+        list | None: list of tuples containing matching run plans, or None.
+    """
+    cursor = con.cursor()
+    if isinstance(search_plan, str):
+        found_plans = cursor.execute(
+            """SELECT plan_no FROM planner.public.plan_names WHERE
+                plan_name = %s;""",
+            (search_plan,),
+        ).fetchall()
+    elif isinstance(search_plan, int):
+        found_plans = cursor.execute(
+            """
+            SELECT plan_name FROM planner.public.plan_names WHERE plan_no = %s
+            """,
+            (search_plan,),
+        ).fetchall()
+    cursor.close()
+    return found_plans
--- backend/tests.py
+++ backend/tests.py
@@ -1,9 +1,11 @@
-import pytest
-from resources import add_run, find_run_func, add_plan
+from resources import add_run, find_run_func, add_plan, find_plan_func
+
 
 add_plan(1, "10k training plan")
+add_plan(2, "Half marathon in 10 weeks")
+assert find_plan_func(2)[0][0] == "Half marathon in 10 weeks"
+assert find_plan_func("Half marathon in 10 weeks")[0][0] == 2
 
 
-def test_add_run():
-    add_run(0, 1, 1, "5km, easy")
-    assert find_run_func(0, 0, 0) == "5km, easy"
+add_run(1, 1, 1, "5km, easy")
+assert find_run_func(1, 1, 1)[0][0] == "5km, easy"
